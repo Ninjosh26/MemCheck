2e33b1c04991b8166683dc91fdd72f73183ad064
Ninjosh26 <70293875+Ninjosh26@users.noreply.github.com>
2022-05-15 00:57:32 -0500

    Added safer access and others
    Changed MemCheck to be a class. Members are now private and can only be accessed with getter functions. Added default constructor for MemCheck. Added the ability to instantiate and utilize your own MemChecks. Added macro to define maximum number of MemCheck objects that can be instantiated in 1 program. Added the ability to enable and disable select MemChecks. Changed name of global MemCheck to be more readable. Changed end-program display toggle to be independent of MemCheck class. Added more examples at the bottom.

--- MemoryCheck.h
+++ MemoryCheck.h
@@ -2,80 +2,147 @@
 #define MEMORYCHECK_H
 #include <iostream>
 
-// Struct that stores information about heap allocation
-struct MemCheck {
-    size_t numAlloc = 0; // Number of total heap allocations
-    size_t numDealloc = 0; // Number of total heap deallocations
-    size_t bytesAlloc = 0; // Total bytes allocated on heap
-    size_t bytesDealloc = 0; // Total bytes deallocated on heap
-    size_t currByteCount = 0; // Curr bytes allocated on the heap
-    bool displayOnDestroy = true; // Defaults to display heap memory remaining on program termination
+// Class that stores information about heap allocation
+class MemCheck {
+    private:
+        size_t _numAlloc;      // Number of total heap allocations
+        size_t _numDealloc;    // Number of total heap deallocations
+        size_t _bytesAlloc;    // Total bytes allocated on heap
+        size_t _bytesDealloc;  // Total bytes deallocated on heap
+        size_t _currByteCount; // Curr bytes allocated on the heap
+        bool _enabled;         // If enabled, a MemCheck object tracks heap usage 
+    public:
+        MemCheck();
 
-    // Function that prints memory information
-    void memStats() {
-        std::cout << "[Number of Allocations]: " << numAlloc << std::endl;
-        std::cout << "[Bytes Allocated]: " << bytesAlloc << std::endl;
-        std::cout << "[Number of Deallocations]: " << numDealloc << std::endl;
-        std::cout << "[Bytes Deallocated]: " << bytesDealloc << std::endl;
-        std::cout << "[Current Bytes Allocated]: " << currByteCount << std::endl << std::endl;
-    }
-
-    // Use to change if message should be printed after program terminates
-    void display(bool b) {
-        displayOnDestroy = b;
-    }
-
-    ~MemCheck() {
-        if (!displayOnDestroy) {
-            return;
+        // Function that prints memory information
+        void memStats() {
+            std::cout << "[Number of Allocations]: " << _numAlloc << std::endl;
+            std::cout << "[Bytes Allocated]: " << _bytesAlloc << std::endl;
+            std::cout << "[Number of Deallocations]: " << _numDealloc << std::endl;
+            std::cout << "[Bytes Deallocated]: " << _bytesDealloc << std::endl;
+            std::cout << "[Current Bytes Allocated]: " << _currByteCount << std::endl << std::endl;
         }
 
-        if (currByteCount) {
-            std::cout << "\033[31m[Memory Not Freed]: " << currByteCount << " bytes not freed" << std::endl;
-        } else {
-            std::cout << "\033[32m[All Memory Freed]" << std::endl;
-        }
-    }
+        // Enable heap memory tracking
+        void enable() { _enabled = true; }
+
+        // Disable heap memory tracking
+        void disable() { _enabled = false; }
+
+        ~MemCheck();
+
+        size_t numAlloc() { return _numAlloc; }
+        size_t numDealloc() { return _numDealloc; }
+        size_t bytesAlloc() { return _bytesAlloc; }
+        size_t bytesDealloc() { return _bytesDealloc; }
+        size_t currByteCount() { return _currByteCount; }
+        bool enabled() { return _enabled; }
+
+        friend void* operator new (size_t size);
+        friend void* operator new[] (size_t size);
+        friend void operator delete (void* memory, size_t size);
+        friend void operator delete[] (void* memory, size_t size);
 };
 
-static MemCheck mc;
+// Maximum number of MemChecks for a program is set to 30 by default
+// Change the macro definition if needed
+#ifndef MAX_MEMCHECKS
+#define MAX_MEMCHECKS 30
+#endif
+
+// Global memory check for the life of the program
+// Using enable and disable with fullProgMem may lead to unintended behavior
+static MemCheck fullProgMem;
+static bool displayOnDestroy = true; // Display heap status on program termination
+static MemCheck* mems[MAX_MEMCHECKS];
+static size_t memCount = 0;
+
+MemCheck::MemCheck(): 
+    _numAlloc(0), 
+    _numDealloc(0), 
+    _bytesAlloc(0), 
+    _bytesDealloc(0), 
+    _currByteCount(0), 
+    _enabled(true) 
+{
+    if (memCount == MAX_MEMCHECKS) {
+        throw std::runtime_error("Exceeds maximum number of MemChecks");
+    }
+
+    mems[memCount] = this;
+    memCount++;     
+}
+
+MemCheck::~MemCheck() {
+    // Only display end behavior if for global MemCheck and display is enabled
+    if (this != &fullProgMem || !displayOnDestroy) {
+        return;
+    }
+
+    // Display memory status on program end
+    if (_currByteCount) {
+        std::cout << "\033[31m[Memory Not Freed]: " << _currByteCount << " bytes not freed" << std::endl;
+    } else {
+        std::cout << "\033[32m[All Memory Freed]" << std::endl;
+    }
+}
+
+// Use to change if message should be printed after program terminates
+void MemCheckDisplay(bool b) {
+    displayOnDestroy = b;
+}
 
 void* operator new (size_t size) {
-    mc.numAlloc++;
-    mc.bytesAlloc += size;
-    mc.currByteCount += size;
-
     if (size == 0) {
-        size++; // avoid std::malloc(0) which may return nullptr on success
+        size++; // Avoid std::malloc(0) which may return nullptr on success
+    }
+
+    // Update info for all enabled MemChecks
+    for (size_t i = 0; i < memCount; i++) {
+        if (mems[i]->enabled()) {
+            mems[i]->_numAlloc++;
+            mems[i]->_bytesAlloc += size;
+            mems[i]->_currByteCount += size;
+        }
     }
 
     if (void *ptr = std::malloc(size)) {
         return ptr;
     }
 
-    throw std::bad_alloc{}; // required by [new.delete.single]/3
+    throw std::bad_alloc{}; // Required by [new.delete.single]/3
 }
 
 void* operator new[] (size_t size) {
-    mc.numAlloc++;
-    mc.bytesAlloc += size;
-    mc.currByteCount += size;
-
     if (size == 0) {
-        size++; // avoid std::malloc(0) which may return nullptr on success
+        size++; // Avoid std::malloc(0) which may return nullptr on success
+    }
+
+    // Update info for all enabled MemChecks
+    for (size_t i = 0; i < memCount; i++) {
+        if (mems[i]->enabled()) {
+            mems[i]->_numAlloc++;
+            mems[i]->_bytesAlloc += size;
+            mems[i]->_currByteCount += size;
+        }
     }
 
     if (void *ptr = std::malloc(size)) {
         return ptr;
     }
 
-    throw std::bad_alloc{}; // required by [new.delete.single]/3
+    throw std::bad_alloc{}; // Required by [new.delete.single]/3
 }
 
 void operator delete (void* memory, size_t size) {
-    mc.numDealloc++;
-    mc.bytesDealloc += size;
-    mc.currByteCount -= size;
+    // Update info for all enabled MemChecks
+    for (size_t i = 0; i < memCount; i++) {
+        if (mems[i]->enabled()) {
+            mems[i]->_numDealloc++;
+            mems[i]->_bytesDealloc += size;
+            mems[i]->_currByteCount -= size;
+        }
+    }
 
     std::free(memory);
 }
@@ -83,20 +150,25 @@ void operator delete (void* memory, size_t size) {
 void operator delete(void* memory) { std::free(memory); }
 
 void operator delete[] (void* memory, size_t size) {
-    mc.numDealloc++;
-    mc.bytesDealloc += size;
-    mc.currByteCount -= size;
+    // Update info for all enabled MemChecks
+    for (size_t i = 0; i < memCount; i++) {
+        if (mems[i]->enabled()) {
+            mems[i]->_numDealloc++;
+            mems[i]->_bytesDealloc += size;
+            mems[i]->_currByteCount -= size;
+        }
+    }
 
     std::free(memory);
 }
 
 void operator delete[] (void* memory) { std::free(memory); }
 
-//////////////////////////////////
-//                              //
-// Sample Program with MemCheck //
-//                              //
-//////////////////////////////////
+///////////////////////////////////
+//                               //
+// Sample Programs with MemCheck //
+//                               //
+///////////////////////////////////
 
 /*
 
@@ -109,12 +181,25 @@ struct Object {
 int main() {
     Object* o = new Object[3];
 
-    mc.memStats();
+    fullProgMem.memStats();
 
     delete[] o;
 
-    mc.memStats();
+    fullProgMem.memStats();
 }
+
+// For more specific behavior, you can create your own MemCheck objects.
+// You can enable and disable them whenever you chose, and disable program-end 
+// display of heap status.
+// Ex)
+
+    MemCheckDisplay(false);    <- No display of heap status
+
+    MemCheck m;
+    int* arr = new int[4];
+    m.disable();               <- Disable heap monitoring
+
+    double* x = new double[3]; <- This is not counted by m 
 
 */
 
